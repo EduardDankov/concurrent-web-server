7c8a55ed8134a7d1e3a8def9935dc5b0c0b54fe6
Eduard Dankov <eduard.dankov@gmail.com>
2024-06-20 14:57:52 +0300

    feat: add socket-related constants
    This adds socket-related constants to constants namespace.

--- server/constants.h
+++ server/constants.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <filesystem>
 
 namespace constants
@@ -5,4 +6,10 @@ namespace constants
 	const std::filesystem::path logDirPath = "logs";
 	const std::filesystem::path requestLogPath = logDirPath / "request_log.txt";
 	const std::filesystem::path responseLogPath = logDirPath / "response_log.txt";
+
+	const std::string serverAddr = "127.0.0.1";
+	const int serverPort = 3579;
+	const size_t threadPoolSize = 16;
+	const size_t backlogSize = 128;
+	const size_t bufferSize = 4096;
 }
