2afd0ab037a6560c1d03adab7bcc8426a9564501
Eduard Dankov <eduard.dankov@gmail.com>
2024-06-21 15:46:52 +0300

    feat: add variable locking to thread pool
    This adds mutex and condition variable to manage access to connection data inside thread pool.

--- server/networking/ThreadPool.cpp
+++ server/networking/ThreadPool.cpp
@@ -4,7 +4,7 @@ networking::ThreadPool::ThreadPool()
 {
 	for (auto& thread : m_threads)
 	{
-		thread = std::thread([this]() { checkForConnections(); });
+		thread = std::thread([this]() { checkForConnections(); } );
 	}
 }
 
@@ -16,15 +16,20 @@ std::shared_ptr<networking::ThreadPool> networking::ThreadPool::getInstance()
 
 void networking::ThreadPool::checkForConnections()
 {
-	if (!m_requestQueue.empty())
+	while (true)
 	{
+		std::unique_lock<std::mutex> lock(m_queueMutex);
+		m_queueCondition.wait(lock, [this]() { return !m_requestQueue.empty(); });
 		std::shared_ptr<Connection> connection = m_requestQueue.front();
 		m_requestQueue.pop();
+		m_queueCondition.notify_one();
 		networking::Socket::getInstance()->handleConnection(connection);
 	}
 }
 
 void networking::ThreadPool::queueConnection(std::shared_ptr<Connection> connection)
 {
+	std::unique_lock<std::mutex> lock(m_queueMutex);
 	m_requestQueue.push(connection);
+	m_queueCondition.notify_one();
 }
--- server/networking/ThreadPool.h
+++ server/networking/ThreadPool.h
@@ -2,6 +2,8 @@
 #include <array>
 #include <queue>
 #include <thread>
+#include <mutex>
+#include <condition_variable>
 #include <memory>
 
 #include "Socket.h"
@@ -15,6 +17,9 @@ namespace networking
 	private:
 		std::array<std::thread, constants::threadPoolSize> m_threads{};
 		std::queue<std::shared_ptr<Connection>> m_requestQueue{};
+
+		std::mutex m_queueMutex;
+		std::condition_variable m_queueCondition;
 
 		ThreadPool();
 
