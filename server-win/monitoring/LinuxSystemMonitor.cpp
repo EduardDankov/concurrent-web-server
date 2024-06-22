7300f11418aa779ad00e7ead3348a2e4fe6e764d
Eduard Dankov <eduard.dankov@gmail.com>
2024-06-22 04:02:01 +0300

    feat: add Linux CPU load calculation
    This implements similar to Windows functionality for getting CPU load in LinuxSystemMonitor.

--- server-win/monitoring/LinuxSystemMonitor.cpp
+++ server-win/monitoring/LinuxSystemMonitor.cpp
@@ -1,7 +1,12 @@
 #include "LinuxSystemMonitor.h"
 
 #include <string>
+#include <fstream>
 #include <cmath>
+#include <vector>
+#include <numeric>
+#include <sys/types.h>
+#include <sys/sysinfo.h>
 
 double monitoring::LinuxSystemMonitor::convertToGB(unsigned long long bytes)
 {
@@ -10,11 +15,59 @@ double monitoring::LinuxSystemMonitor::convertToGB(unsigned long long bytes)
 	return gigabytes;
 }
 
-monitoring::LinuxSystemMonitor::LinuxSystemMonitor() {}
+bool monitoring::LinuxSystemMonitor::getSystemTimes(unsigned long long& idleTime, unsigned long long& totalTime)
+{
+	std::vector<unsigned long long> times;
+	std::ifstream procStat("/proc/stat");
+	procStat.ignore(5, ' '); // skip 'cpu' prefix
+
+	unsigned long long value{};
+	while (procStat >> value)
+	{
+		times.push_back(value);
+	}
+
+	if (times.size() < 4) return false;
+	idleTime = times.at(3);
+	totalTime = std::accumulate(times.begin(), times.end(), 0);
+}
+
+float monitoring::LinuxSystemMonitor::calculateCpuLoad(unsigned long long idleTicks, unsigned long long totalTicks)
+{
+	static unsigned long long s_previousTotalTicks = 0;
+	static unsigned long long s_previousIdleTicks = 0;
+
+	unsigned long long totalTicksSinceLastTime = totalTicks - s_previousTotalTicks;
+	unsigned long long idleTicksSinceLastTime = idleTicks - s_previousIdleTicks;
+
+	float ret = 1.0f - (
+		(totalTicksSinceLastTime > 0)
+		? (static_cast<float>(idleTicksSinceLastTime)) / totalTicksSinceLastTime
+		: 0);
+
+	s_previousTotalTicks = totalTicks;
+	s_previousIdleTicks = idleTicks;
+	return ret;
+}
+
+void monitoring::LinuxSystemMonitor::updateCpuLoad()
+{
+	unsigned long long idleTime, totalTime;
+	while (true)
+	{
+		m_cpuLoad = getSystemTimes(idleTime, totalTime)
+			? calculateCpuLoad(idleTime, totalTime)
+			: -1.0f;
+		usleep(s_intervalBetweenCpuUpdatesMs * 1000);
+	}
+}
+
+monitoring::LinuxSystemMonitor::LinuxSystemMonitor() : m_cpuLoad(0), m_cpuUpdateThread([this]() { updateCpuLoad(); }) {}
 
 std::string monitoring::LinuxSystemMonitor::getCpuLoad()
 {
-	return "-1%";
+	float currentLoad = std::round(m_cpuLoad * 100 * 100) / 100;
+	return std::to_string(currentLoad) + '%';
 }
 
 std::string monitoring::LinuxSystemMonitor::getMemoryUsage()
--- server-win/monitoring/LinuxSystemMonitor.h
+++ server-win/monitoring/LinuxSystemMonitor.h
@@ -2,15 +2,22 @@
 
 #ifdef __linux__
 #include <string>
-#include <sys/types.h>
-#include <sys/sysinfo.h>
+#include <thread>
 
 namespace monitoring
 {
 	class LinuxSystemMonitor
 	{
 	protected:
+		static const time_t s_intervalBetweenCpuUpdatesMs{ 100 };
+		float m_cpuLoad;
+		std::thread m_cpuUpdateThread;
+
 		double convertToGB(unsigned long long bytes);
+
+		bool getSystemTimes(unsigned long long& idleTime, unsigned long long& totalTime);
+		float calculateCpuLoad(unsigned long long idleTicks, unsigned long long totalTicks);
+		void updateCpuLoad();
 
 	public:
 		LinuxSystemMonitor();
